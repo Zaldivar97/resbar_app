8a4d684a7d2e899f3ff13b5399c6c8d75ec20ac5
Ngochez95 <nhgochez95@gmail.com>
2018-05-18 01:25:18 -0600

    ManejadorParametros

--- ResBarLib/src/sv/edu/uesocc/disenio2018/resbar/backend/controller/ManejadorParametros.java
+++ ResBarLib/src/sv/edu/uesocc/disenio2018/resbar/backend/controller/ManejadorParametros.java
@@ -5,8 +5,15 @@
  */
 package sv.edu.uesocc.disenio2018.resbar.backend.controller;
 
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import static sv.edu.uesocc.disenio2018.resbar.backend.controller.ManejadorProductos.getEM;
+import sv.edu.uesocc.disenio2018.resbar.backend.controller.exceptions.ErrorApplication;
+import sv.edu.uesocc.disenio2018.resbar.backend.entities.Parametro;
+import sv.edu.uesocc.disenio2018.resbar.backend.entities.Producto;
 
 /**
  *
@@ -17,7 +24,55 @@ public class ManejadorParametros {
     protected static EntityManager getEM() {
         return Persistence.createEntityManagerFactory("ResbarBackendPU").createEntityManager();
     }
-    
-    
+
+    protected static void actualizar(Parametro entityObject) {
+        EntityManager eml = getEM();
+        EntityTransaction et = eml.getTransaction();
+        try {
+            if (!et.isActive()) {
+                et.begin();
+            }
+            eml.merge(entityObject);
+            et.commit();
+        } catch (Exception ex) {
+            if (et.isActive()) {
+                et.rollback();
+            }
+            throw new ErrorApplication("Fallo actualizar el parámetro --> $ManejadorPaŕametros.actualizar() --> " + ex.getMessage());
+        } finally {
+            if (eml.isOpen()) {
+                eml.close();
+            }
+
+        }
+    }
+
+    protected static Object obtener(Integer id) {
+        EntityManager eml = getEM();
+        try {
+            return eml.find(Producto.class, id);
+        } catch (Exception e) {
+            return null;
+        } finally {
+            if (eml.isOpen()) {
+                eml.close();
+            }
+
+        }
+    }
+
+    protected static List<Parametro> obtener() {
+        EntityManager eml = getEM();
+        try {
+            Query query = eml.createNamedQuery("Parametro.findAll");
+            return query.getResultList();
+        } catch (Exception ex) {
+            throw new ErrorApplication("Fallo obtener lista de Parámetros --> $ManejadorParámetros --> " + ex.getMessage());
+        } finally {
+            if (eml.isOpen()) {
+                eml.close();
+            }
+        }
+    }
 
 }
